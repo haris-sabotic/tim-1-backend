729e3b6420ccd3a50203f99f664ea5f15410cc7a
haris-sabotic <haris.sinfo16@gmail.com>
2023-10-14 20:00:30 +0200

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('authToken')->accessToken;
+
+            return response()->json(['success' => $success], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $validatedData = $validator->validated();
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        $success['user'] = $user;
+        $success['token'] = $accessToken;
+        $success['message'] = "Account created successfully.";
+        return response()->json(['success' => $success], 200);
+    }
+
+    public function details()
+    {
+        $user = Auth::user();
+        return response()->json(['success' => $user], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+ */
+
+Route::post('login', [Controllers\UserController::class, 'login']);
+Route::post('register', [Controllers\UserController::class, 'register']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('details', [Controllers\UserController::class, 'details'])->name("details");
+});
