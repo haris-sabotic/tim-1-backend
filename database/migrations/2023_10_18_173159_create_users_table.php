fbcf6571f3b9acf9198436a5768c95cfa98dbb4b
haris-sabotic <haris.sinfo16@gmail.com>
2023-10-21 15:56:23 +0200

    Add admin field to user

--- database/migrations/2023_10_18_173159_create_users_table.php
+++ database/migrations/2023_10_18_173159_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('photo')->nullable();
+            $table->boolean('admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
