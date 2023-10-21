d7ff302f14ea0001b5ad937db675fa82967c8d62
haris-sabotic <haris.sinfo16@gmail.com>
2023-10-21 22:43:36 +0200

    Add filter to GET articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,8 +7,12 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function all()
+    public function all(Request $request)
     {
+        if ($request->name) {
+            return Article::where('name', 'LIKE', '%' . $request->name . '%')->get()->load("tags");
+        }
+
         return Article::all()->load("tags");
     }
 
